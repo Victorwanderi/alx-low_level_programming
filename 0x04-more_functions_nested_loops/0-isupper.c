f30632f2a209fedf52035e7a70398b167a50df4d
Victorwanderi <wanderivictormuya@gmail.com>
2022-07-07 05:40:25 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _issuper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
+ */
+
+int _issuper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
